d6d5f04eac29a729230b6a4dd56ca575ea022c63
Michael East <michael@michaeleast.dev>
2022-08-09 17:19:44 +0100

    Fetch is now working, and styles have been updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,24 @@
-const adviceNumber = document.querySelector('.advice-number');
-const adviceText = document.querySelector('.advice-text');
+const adviceNumber = document.querySelector('.advice-number-api');
+const adviceText = document.querySelector('.advice-text-api');
+const diceButton = document.querySelector('.dice-container');
+
+const defaultQuote = `It is easy to sit up and take notice, what's difficult is getting up and taking action.`;
+const defaultNumber = `117`;
+
+const fetchUrl = `https://api.adviceslip.com/advice`;
+
+const getAdvice = () => {
+  fetch(fetchUrl)
+    .then((r) => r.json())
+    .then((data) => {
+      console.log(data.slip);
+      adviceNumber.innerHTML = data.slip.id;
+      adviceText.innerHTML = data.slip.advice;
+    });
+};
+
+getAdvice();
+
+diceButton.addEventListener('click', () => {
+  getAdvice();
+});
